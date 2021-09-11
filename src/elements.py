a2c9e0b0f130185bbe360b91ba7df234a3e172d9
Federico Santana <fede.santana@hotmail.com>
2021-09-11 10:29:34 -0300

    add sources and array for Nodes

--- src/elements.py
+++ src/elements.py
@@ -2,15 +2,23 @@ from sympy import symbols
 from sympy import Symbol # https://github.com/sympy/sympy/blob/master/sympy/core/symbol.py
 from sympy import I
 from typing import Optional 
+from ctypes import Array
 
 # The error from Mypy to sympy library can be fixed, we have to read this issue https://github.com/sympy/sympy/pull/18244
 class Nodo:
-    def __init__(self, name: str):
+    def __init__(self, name: str, elements: Array[BasicComponent]):
         self.name = name
+        self.elements : Array[BasicComponent] = []
+    def addElement (self, component : BasicComponent):
+        self.elements.push(component)
 
 class Orientation:
     def __init__(self, name: str):
         self.name = "positive"
+
+class ground:
+    def __init (self, nodo : Nodo):
+        self.nodo = nodo
 
 class BasicComponent:
     def __init__(
@@ -27,11 +35,37 @@ class BasicComponent:
             if (value is not None):
                 if (value > 0):
                     self.value = value
-                else if (value <= 0):
+                elif (value <= 0):
                     self.doable = False
                 else:
                     raise ValueError("Sorry, the value have to be a number")
             self.orientation = orientation
+
+class voltageSource(BasicComponent):
+    def __init__(
+        self,
+        name: str, 
+        nodo1: Nodo, 
+        nodo2: Nodo, 
+        value: Optional[float] = None, # If is Float can be int, here why: https://www.python.org/dev/peps/pep-0484/#the-numeric-tower
+        orientation : Optional[Orientation] = None
+        ):
+            super().__init__(name, nodo1, nodo2, value)
+            self.orientation = None
+            self.s = symbols('s', positive=True) 
+
+class currentSource(BasicComponent):
+    def __init__(
+        self,
+        name: str, 
+        nodo1: Nodo, 
+        nodo2: Nodo, 
+        value: Optional[float] = None, # If is Float can be int, here why: https://www.python.org/dev/peps/pep-0484/#the-numeric-tower
+        orientation : Optional[Orientation] = None
+        ):
+            super().__init__(name, nodo1, nodo2, value)
+            self.orientation = None
+            self.s = symbols('s', positive=True) 
 
 class PassiveComponent(BasicComponent):
     def __init__(
